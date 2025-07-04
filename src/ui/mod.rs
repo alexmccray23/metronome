de7a1690d5475a176b5c22b68bf572862a93f0b9
alexmccray23 <alex.mccray@gmail.com>
2025-07-04 11:42:57 -0400

    Add tempo-setting text field

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -20,6 +20,8 @@ pub struct AppState {
     current_bpm: f64,
     state: MetronomeState,
     tap_tempo: TapTempo,
+    input_mode: bool,
+    input_buffer: String,
 }
 
 impl AppState {
@@ -30,47 +32,97 @@ impl AppState {
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         if event::poll(Duration::from_millis(16))? {
             if let Event::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Char('k') => {
-                        let mut bpm = bpm_shared.lock().unwrap();
-                        *bpm += 1.0;
-                        self.current_bpm = *bpm;
-                    }
-                    KeyCode::Char('j') => {
-                        let mut bpm = bpm_shared.lock().unwrap();
-                        if *bpm > 1.0 {
-                            *bpm -= 1.0;
-                            self.current_bpm = *bpm;
-                        }
-                    }
-                    KeyCode::Char('q') => {
-                        self.state = MetronomeState::Stopped;
-                        state.store(MetronomeState::Stopped, Ordering::SeqCst);
-                    }
-                    KeyCode::Char(' ') => {
-                        let current_state = state.load(Ordering::SeqCst);
-                        let new_state = match current_state {
-                            MetronomeState::Running => MetronomeState::Paused,
-                            MetronomeState::Paused => MetronomeState::Running,
-                            MetronomeState::Stopped => MetronomeState::Stopped,
-                        };
-                        state.store(new_state, Ordering::SeqCst);
-                        self.state = new_state;
-                    }
-                    KeyCode::Char('g' | 'G') => {
-                        if let Some(bpm) = self.tap_tempo.tap() {
-                            {
-                                let mut shared_bpm = bpm_shared.lock().unwrap();
-                                *shared_bpm = bpm;
-                            }
-                            self.current_bpm = bpm;
-                        }
-                    }
-                    _ => {}
+                if self.input_mode {
+                    self.handle_input_mode(key, bpm_shared);
+                } else {
+                    self.handle_normal_mode(key, bpm_shared, state);
                 }
             }
         }
         Ok(())
+    }
+
+    fn handle_normal_mode(
+        &mut self,
+        key: crossterm::event::KeyEvent,
+        bpm_shared: &Arc<Mutex<f64>>,
+        state: &AtomicMetronomeState,
+    ) {
+        match key.code {
+            KeyCode::Char('k') => {
+                let mut bpm = bpm_shared.lock().unwrap();
+                *bpm += 1.0;
+                self.current_bpm = *bpm;
+            }
+            KeyCode::Char('j') => {
+                let mut bpm = bpm_shared.lock().unwrap();
+                if *bpm > 1.0 {
+                    *bpm -= 1.0;
+                    self.current_bpm = *bpm;
+                }
+            }
+            KeyCode::Char('q') => {
+                self.state = MetronomeState::Stopped;
+                state.store(MetronomeState::Stopped, Ordering::SeqCst);
+            }
+            KeyCode::Char(' ') => {
+                let current_state = state.load(Ordering::SeqCst);
+                let new_state = match current_state {
+                    MetronomeState::Running => MetronomeState::Paused,
+                    MetronomeState::Paused => MetronomeState::Running,
+                    MetronomeState::Stopped => MetronomeState::Stopped,
+                };
+                state.store(new_state, Ordering::SeqCst);
+                self.state = new_state;
+            }
+            KeyCode::Char('g' | 'G') => {
+                if let Some(bpm) = self.tap_tempo.tap() {
+                    {
+                        let mut shared_bpm = bpm_shared.lock().unwrap();
+                        *shared_bpm = bpm;
+                    }
+                    self.current_bpm = bpm;
+                }
+            }
+            KeyCode::Char('i' | 'I') | KeyCode::Enter => {
+                self.input_mode = true;
+                self.input_buffer.clear();
+            }
+            _ => {}
+        }
+    }
+
+    fn handle_input_mode(
+        &mut self,
+        key: crossterm::event::KeyEvent,
+        bpm_shared: &Arc<Mutex<f64>>,
+    ) {
+        match key.code {
+            KeyCode::Enter => {
+                if let Ok(bpm) = self.input_buffer.parse::<f64>() {
+                    if bpm > 0.0 {
+                        {
+                            let mut shared_bpm = bpm_shared.lock().unwrap();
+                            *shared_bpm = bpm;
+                        }
+                        self.current_bpm = bpm;
+                    }
+                }
+                self.input_mode = false;
+                self.input_buffer.clear();
+            }
+            KeyCode::Esc => {
+                self.input_mode = false;
+                self.input_buffer.clear();
+            }
+            KeyCode::Backspace => {
+                self.input_buffer.pop();
+            }
+            KeyCode::Char(c) if c.is_ascii_digit() || c == '.' => {
+                self.input_buffer.push(c);
+            }
+            _ => {}
+        }
     }
 }
 
@@ -89,14 +141,23 @@ pub async fn run(
         current_bpm: start_bpm,
         state: state.load(Ordering::SeqCst),
         tap_tempo: TapTempo::new(),
+        input_mode: false,
+        input_buffer: String::new(),
     };
 
     while app_state.state != MetronomeState::Stopped {
         terminal.draw(|f| {
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
-                .split(f.area());
+            let chunks = if app_state.input_mode {
+                Layout::default()
+                    .direction(Direction::Vertical)
+                    .constraints([Constraint::Percentage(60), Constraint::Length(3), Constraint::Percentage(37)].as_ref())
+                    .split(f.area())
+            } else {
+                Layout::default()
+                    .direction(Direction::Vertical)
+                    .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
+                    .split(f.area())
+            };
 
             let paused_text = if app_state.state == MetronomeState::Paused {
                 " [PAUSED]".red()
@@ -130,6 +191,27 @@ pub async fn run(
             );
             f.render_widget(bpm_block, chunks[0]);
 
+            // Render input field if in input mode
+            if app_state.input_mode {
+                let input_text = vec![
+                    Line::from(vec![
+                        "Enter BPM: ".into(),
+                        Span::styled(
+                            &app_state.input_buffer,
+                            Style::default().fg(Color::Yellow),
+                        ),
+                        "_".yellow(),
+                    ]),
+                ];
+
+                let input_block = Paragraph::new(input_text).centered().block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .title(Line::from(" Input BPM (Enter to confirm, Esc to cancel) ".cyan().bold()).centered()),
+                );
+                f.render_widget(input_block, chunks[1]);
+            }
+
             let controls_text = vec![
                 Line::from(vec![
                     "Decrease BPM: ".into(),
@@ -144,7 +226,8 @@ pub async fn run(
                 Line::from(vec![
                     "Tap Tempo: ".into(),
                     "<G>".blue(),
-                    " (Press repeatedly to set tempo)".into(),
+                    " Manual Input: ".into(),
+                    "<I>".blue(),
                 ]).centered(),
             ];
 
@@ -153,7 +236,8 @@ pub async fn run(
                     .borders(Borders::ALL)
                     .title(Line::from(" Controls ".yellow().bold()).centered()),
             );
-            f.render_widget(controls_block, chunks[1]);
+            let controls_chunk_index = if app_state.input_mode { 2 } else { 1 };
+            f.render_widget(controls_block, chunks[controls_chunk_index]);
         })?;
 
         if let Ok(new_bpm) = bpm_shared.lock() {
